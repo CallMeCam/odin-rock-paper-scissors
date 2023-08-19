b09c50057d7a386b9c52ba7197afa54a86ea3126
Cam <37002723+CallMeCam@users.noreply.github.com>
2023-08-19 17:15:15 -0400

    Added UI elements
    created UI button elements and text areas to display scores, etc.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 // all possible choices for player and computer
 const PLAY_CHOICES = ["rock", "paper", "scissors"];
 
-// temporarily set user input to Paper for testing
-// don't forget the user input needs to be lowercase
-const userInput = "paper";
+// text to update on page
+const playMessage = document.getElementById('play-message');
+
+// variables for storing winner of last round
+let winnerMessage;
+let winnerNumber;
+
+// track scores and rounds
+let playerWins = 0,
+    computerWins = 0,
+    ties = 0,
+    roundCount = 1,
+    winner;
 
 // get random results from possible choices
 const getComputerChoice = function (choices) {
@@ -23,90 +33,94 @@ const playRound = function (playerSelection, computerSelection) {
 
     // string for winner message, and int for winner indicator to be used for score tracking later.
     // 0 = tie or bad input, 1 = player loses, 2 = player wins. this feels clunky but it should work for now
-    let winnerMessage;
-    let winnerNumber;
-
-    // check that player input is valid
-    if (PLAY_CHOICES.includes(playerSelection)) {
-        // check first if it's a tie, then proceed to determine winner
-        if (playerSelection === computerSelection) {
-            winnerMessage = "It's a tie!";
-            winnerNumber = 0;
-        } else if (playerSelection === "rock") {
-            if (computerSelection === "paper") {
-                winnerMessage = "You lose! Paper beats Rock";
-                winnerNumber = 1;
-            } else if (computerSelection === "scissors") {
-                winnerMessage = "You win! Rock beats Scissors";
-                winnerNumber = 2;
-            }
-        } else if (playerSelection === "paper") {
-            if (computerSelection === "scissors") {
-                winnerMessage = "You lose! Scissors beats Paper";
-                winnerNumber = 1;
-            } else if (computerSelection === "rock") {
-                winnerMessage = "You win! Paper beats Rock";
-                winnerNumber = 2;
-            }
-        } else if (playerSelection === "scissors") {
-            if (computerSelection === "rock") {
-                winnerMessage = "You lose! Rock beats Scissors";
-                winnerNumber = 1;
-            } else if (computerSelection === "paper") {
-                winnerMessage = "You win! Scissors beats Paper";
-                winnerNumber = 2;
-            }
-        }
-    } else {
-        //if player input is not rock, paper, or scissors, return error message
-        winnerMessage = "That's not a valid input. Please try again.";
+    
+    // check first if it's a tie, then proceed to determine winner
+    if (playerSelection === computerSelection) {
+        winnerMessage = "It's a tie!";
         winnerNumber = 0;
+        ties++;
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            winnerMessage = "You lose! Paper beats Rock";
+            winnerNumber = 1;
+            computerWins++;
+        } else if (computerSelection === "scissors") {
+            winnerMessage = "You win! Rock beats Scissors";
+            winnerNumber = 2;
+            playerWins++;
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            winnerMessage = "You lose! Scissors beats Paper";
+            winnerNumber = 1;
+            computerWins++;
+        } else if (computerSelection === "rock") {
+            winnerMessage = "You win! Paper beats Rock";
+            winnerNumber = 2;
+            playerWins++;
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            winnerMessage = "You lose! Rock beats Scissors";
+            winnerNumber = 1;
+            computerWins++;
+        } else if (computerSelection === "paper") {
+            winnerMessage = "You win! Scissors beats Paper";
+            winnerNumber = 2;
+            playerWins++;
+        }
     }
 
-    // check values for correctness
-    //console.log(playerSelection, computerSelection);
+    console.log("Round " + roundCount +" played");
+    console.log("Player choice: " + playerSelection);
+    console.log("Computer choice: " + computerSelection);
+    console.log(winnerMessage);
     return [winnerNumber, winnerMessage];
 };
 
-const game = function () {
-    // initialize winner counts to 0
-    let playerWins = 0,
-        computerWins = 0,
-        ties = 0,
-        roundCount = 0,
-        winner;
+const resetScores = function () {
+    playerWins = 0;
+    computerWins = 0;
+    ties = 0;
+    roundCount = 1;
+    console.log("Scores reset");
+}
 
-    // play a 5-round game
-    for (let i = 0; i < 5; i++) {
-        // call playRound function and check winner value at array[0]. increment winner count
-        // there are probably a million other, better ways to do this
-        if (playRound(userInput, getComputerChoice(PLAY_CHOICES))[0] === 1) {
-            computerWins++;
-            console.log("Computer wins round " + (roundCount+1) + ". Computer: " + computerWins + " Player: " + playerWins);
-        } else if (playRound(userInput, getComputerChoice(PLAY_CHOICES))[0] === 2) {
-            playerWins++;
-            console.log("Player wins round " + (roundCount+1) + ". Computer: " + computerWins + " Player: " + playerWins);
-        } else {
-            ties++;
-            console.log("Round " + (roundCount+1) + " is a tie. Computer: " + computerWins + " Player: " + playerWins);
-        }
-        roundCount++;
-    }
-
+const displayWinner = function () {
     if (playerWins == computerWins) {
-        winner = "It's a tie.";
+        winner = "Game Over! It's a tie.";
     } else if (playerWins > computerWins) {
-        winner = "You win!";
+        winner = "Game Over! You win!";
     } else {
-        winner = "You lose!";
+        winner = "Game Over! You lose!";
     }
 
     return winner;
 }
 
-// get user input and convert to lower case
-//let userInput = prompt("Please enter your choice of rock, paper, or scissors: ").toLowerCase();
+const game = function (playerSelection) {
+    
+    if (roundCount < 5) {
+        playRound(playerSelection, getComputerChoice(PLAY_CHOICES));
+        roundCount++;
+        playMessage.textContent = winnerMessage;
+    } else {
+        // play final round
+        playRound(playerSelection, getComputerChoice(PLAY_CHOICES));
+        roundCount++;
+        playMessage.textContent = winnerMessage;
 
-//console.log(playRound(userInput, getComputerChoice(PLAY_CHOICES)));
+        // determine and display winner of series, then reset scores for next round
+        displayWinner();
+        playMessage.textContent = winner;
+        resetScores();
+    }
+}
 
-console.log(game());
+// adding events to play buttons
+const selectionButton = document.getElementsByClassName('selectionButton');
+const buttonArray = [...selectionButton];
+
+buttonArray.forEach(button => button.addEventListener('click', (e) => {
+    game(button.id);
+}));
